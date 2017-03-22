77ea7cf7c03bc04b915dc71ef33a935cb0774a52
Ian Wordes <iwordes@student.42.us.org>
2017-03-21 19:28:18 -0700

    Add `E: ` prefix to errors

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/21 18:16:22 by iwordes           #+#    #+#             */
-/*   Updated: 2017/03/21 18:21:02 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/03/21 19:26:19 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 void	error(const char *msg)
 {
-	ft_eprintf("\e[91m%s\e[0m\n", msg);
+	ft_eprintf("E: \e[91m%s\e[0m\n", msg);
 	exit(1);
 }
